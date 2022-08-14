8060b96f68a3fb3776bd496b781fa8c13bcf6db9
Miguel <barrigamiguelm@gmail.com>
2022-08-14 21:29:02 +0200

    "Funcionando todo"

--- app/src/main/java/com/mbarmed/dragbeer/CreacionUsuario.java
+++ app/src/main/java/com/mbarmed/dragbeer/CreacionUsuario.java
@@ -1,59 +1,45 @@
 package com.mbarmed.dragbeer;
 
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.app.Activity;
 import android.app.DatePickerDialog;
-import android.content.ContentResolver;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-import android.os.UserHandle;
 import android.text.TextUtils;
 import android.view.View;
-import android.webkit.MimeTypeMap;
 import android.widget.Button;
-import android.widget.DatePicker;
 import android.widget.EditText;
-import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.auth.UserProfileChangeRequest;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
-import com.squareup.picasso.Picasso;
-
-import java.time.LocalDate;
-import java.time.Period;
-import java.util.Calendar;
-import java.util.Date;
 
 public class CreacionUsuario extends AppCompatActivity {
 
-    private static final int PICK_IMAGE_REQUEST = 1;
-    private TextView tvSaltar;
-    private ImageView imageView;
-    private EditText dateUser, username, emailCrear, biografia;
-    private Button btnRegistrar, uploadPhoto;
-
-    private DatabaseReference mDatabase;
+    private EditText dateUser;
+    private EditText username;
+    private EditText biografia;
     private FirebaseAuth mAuth;
-    private FirebaseStorage storage;
-    private StorageReference mStorage;
-    private FirebaseUser firebaseUser;
-    private Uri uriImage;
-
-    FirebaseUser user;
-    DatePickerDialog.OnDateSetListener setListener;
+    private ImageView fotoPerfil;
+    Uri imageUri;
+    StorageReference storage;
+    FirebaseUser firebaseUser;
+    String email;
+    DatabaseReference mDatabase;
 
 
     @Override
@@ -62,76 +48,54 @@ public class CreacionUsuario extends AppCompatActivity {
         setContentView(R.layout.activity_creacion_usuario);
 
         //inicializar
-        tvSaltar = (TextView) findViewById(R.id.tvSaltar);
-        imageView = findViewById(R.id.imageView);
+        TextView tvSaltar = (TextView) findViewById(R.id.tvSaltar);
         dateUser = findViewById(R.id.dateUser);
+        fotoPerfil = findViewById(R.id.fotoPerfil);
         username = findViewById(R.id.username);
-        emailCrear = findViewById(R.id.emailCrear);
-        btnRegistrar = findViewById(R.id.btnRegistrar);
-        uploadPhoto = findViewById(R.id.uploadPhoto);
+        EditText emailCrear = findViewById(R.id.emailCrear);
+        Button btnRegistrar = findViewById(R.id.btnRegistrar);
         biografia = findViewById(R.id.biografia);
 
         //Firebase
         mDatabase = FirebaseDatabase.getInstance("https://dragbeer-83331-default-rtdb.europe-west1.firebasedatabase.app/").getReference();
         mAuth = FirebaseAuth.getInstance();
         firebaseUser = mAuth.getCurrentUser();
-        mStorage = FirebaseStorage.getInstance("gs://dragbeer-83331.appspot.com").getReference("Profile pics");
 
+        fotoPerfil.setImageDrawable(getResources().getDrawable(R.drawable.perfil));
 
         //autocomplete email with firebase authentication
         if (firebaseUser != null) {
-            String correo = firebaseUser.getEmail();
-            emailCrear.setText(correo);
+            email = firebaseUser.getEmail();
+            emailCrear.setText(email);
         }
 
-        //Profile picture
-        Uri uri = firebaseUser.getPhotoUrl();
-
-        Picasso.with(CreacionUsuario.this).load(uri).into(imageView);
-
-        uploadPhoto.setOnClickListener(new View.OnClickListener() {
+        fotoPerfil.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                openFile();
+                selectImage();
             }
         });
+
+
 
         btnRegistrar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                uploadPhoto();
-               // mandarDatos();
+                mandarDatos(email);
+                uploadImage();
             }
         });
 
 
-    }
 
-    private void openFile() {
-        Intent intent = new Intent();
-        intent.setType("image/*");
-        intent.setAction(Intent.ACTION_GET_CONTENT);
-        startActivityForResult(intent, PICK_IMAGE_REQUEST);
-
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == PICK_IMAGE_REQUEST && resultCode == Activity.RESULT_OK && data != null && data.getData() != null) {
-            uriImage = data.getData();
-            imageView.setImageURI(uriImage);
-
-        }
     }
 
     //mandar datos a firebase
-    private void mandarDatos() {
+    private void mandarDatos(String email) {
 
         String usuario = username.getText().toString();
         String bio = biografia.getText().toString();
         String age = dateUser.getText().toString();
-
 
         if (TextUtils.isEmpty(usuario)) {
             username.requestFocus();
@@ -141,43 +105,61 @@ public class CreacionUsuario extends AppCompatActivity {
             dateUser.requestFocus();
             dateUser.setError("Pon tu edad");
         } else {
-
-        }
-
-    }
-
-    //subir foto a firebase
-    private void uploadPhoto() {
-
-        if (uriImage == null) {
-            StorageReference fileReference = mStorage.child(mAuth.getCurrentUser().getUid() + "." +
-                    getFileExtension(uriImage));
-
-            fileReference.putFile(uriImage).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+            ReadWriteUserDetails user = new ReadWriteUserDetails(usuario, bio, age, email);
+            mDatabase.child(firebaseUser.getUid()).setValue(user).addOnCompleteListener(new OnCompleteListener<Void>() {
                 @Override
-                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
-                    fileReference.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
-                        @Override
-                        public void onSuccess(Uri uriImage) {
-                            Uri downloaUri = uriImage;
-                            firebaseUser = mAuth.getCurrentUser();
-                            UserProfileChangeRequest profileUpdates = new UserProfileChangeRequest.Builder()
-                                    .setPhotoUri(downloaUri).build();
-                            firebaseUser.updateProfile(profileUpdates);
-                            Toast.makeText(CreacionUsuario.this, "He llegado", Toast.LENGTH_SHORT).show();
-                        }
-                    });
+                public void onComplete(@NonNull Task<Void> task) {
+                    if (task.isSuccessful()){
+                        Toast.makeText(CreacionUsuario.this, "Funcionado ( borrame ) ", Toast.LENGTH_SHORT).show();
+                        Intent intent = new Intent(CreacionUsuario.this,Etiquetas.class);
+                        // intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+                        // startActivity(intent);
+                        // finish();
+                    }else{
+                        Toast.makeText(CreacionUsuario.this, "Hubo un problema, porfavor intenta de nuevo", Toast.LENGTH_SHORT).show();
+                    }
+
                 }
             });
         }
     }
 
-    private String getFileExtension(Uri uri) {
-        ContentResolver cr = this.getContentResolver();
-        MimeTypeMap mime = MimeTypeMap.getSingleton();
-        return mime.getExtensionFromMimeType(cr.getType(uri));
+    private void uploadImage() {
+
+        storage = FirebaseStorage.getInstance("gs://dragbeer-83331.appspot.com/").getReference("FotosPerfil");
+
+        storage.child(mAuth.getCurrentUser().getUid()).putFile(imageUri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                Toast.makeText(CreacionUsuario.this, "Se ha mandado", Toast.LENGTH_SHORT).show();
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(CreacionUsuario.this, "Erroreees", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
+    private void selectImage() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(intent, 100);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == 100 && data != null && data.getData() != null) {
+
+            imageUri = data.getData();
+
+            fotoPerfil.setImageURI(imageUri);
+
+        }
+    }
 
 }
 
---
+++ app/src/main/java/com/mbarmed/dragbeer/ReadWriteUserDetails.java
@@ -0,0 +1,13 @@
+package com.mbarmed.dragbeer;
+
+public class ReadWriteUserDetails {
+    public String usuario,bio,age,email;
+
+    public ReadWriteUserDetails(String usuario, String bio, String age, String email) {
+        this.usuario = usuario;
+        this.bio = bio;
+        this.age = age;
+        this.email = email;
+    }
+
+}
